a69d50984c5f9cbf3e37672f0ced58cfd7d0a07a
muslimunicode <btsokaev@mail.ru>
2023-07-13 16:15:39 +0300

    step 14

---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+import "./styles/index.scss"
+import { classNames } from "shared/lib/classNames/classNames";
+import { useTheme } from "app/providers/ThemeProvider";
+import { AppRouter } from "./providers/router";
+import { Navbar } from "widgets/Navbar";
+import { Sidebar } from "widgets/Sidebar";
+
+export const App = () => {
+    const { theme } = useTheme()
+
+    return (
+        <div className={classNames('app', {}, [theme])}>
+            <Navbar />
+            <div className="content-page">
+                <Sidebar />
+                <AppRouter />
+            </div>
+        </div>
+    );
+}
---
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -0,0 +1,23 @@
+import cls from "./Navbar.module.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
+import { ThemeSwitcher } from "widgets/ThemeSwitcher";
+
+interface NavbarProps {
+    className?: string;
+}
+
+export const Navbar = ({className}: NavbarProps) => {
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink to={"/"} theme={AppLinkTheme.SECONDARY} className={cls.mainLink}>
+                    Main
+                </AppLink>
+                <AppLink to={"/about"} theme={AppLinkTheme.SECONDARY}>
+                    About
+                </AppLink>
+            </div>
+        </div>
+    );
+}
