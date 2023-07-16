36d5809c20a6bb9b68d87f8a676e62cbe444fd3b
muslimunicode <btsokaev@mail.ru>
2023-07-16 12:48:07 +0300

    step 15

--- src/pages/AboutPage/ui/AboutPage.tsx
+++ src/pages/AboutPage/ui/AboutPage.tsx
@@ -1,7 +1,11 @@
+import { useTranslation } from "react-i18next";
+
 const AboutPage = () => {
+    const {t} = useTranslation('about')
+
     return (
         <div className="about-page">
-            About Page
+            {t('О сайте')}
         </div>
     );
 }
