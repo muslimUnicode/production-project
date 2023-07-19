479303805c86979cac8db8205d4d41fb0c559560
muslimunicode <btsokaev@mail.ru>
2023-07-19 12:25:42 +0300

    add jest, add first test

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     env: {
         browser: true,
         es2021: true,
+        jest: true,
     },
     extends: [
         'plugin:react/recommended',
@@ -39,6 +40,7 @@ module.exports = {
         'no-underscore-dangle': 'off',
         'no-undef': 'off',
         'i18next/no-literal-string': ['warn', { markupOnly: true }],
+        'max-len': ['error', { code: 100, ignoreComments: true }],
     },
     globals: {
         __IS_DEV__: true,
--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -1,9 +1,9 @@
 type Mods = Record<string, boolean | string>
 
-export const classNames = (cls: string, mods?:Mods, additional?: string[]): string => [
+export const classNames = (cls: string, mods:Mods = {}, additional: string[] = []): string => [
     cls,
     ...additional.filter(Boolean),
-    Object.entries(mods)
+    ...Object.entries(mods)
         .filter(([classNames, value]) => Boolean(value))
         .map(([className]) => className),
 ].join(' ');
